e99da8b43db8c2ecd3b28d378f33748cd2a7257c
makotoyoshigoe <myoshigo0127@gmail.com>
2022-10-08 23:11:44 +0900

    272c修正

--- ABC/272/c.cpp
+++ ABC/272/c.cpp
@@ -5,11 +5,9 @@
 using namespace std;
 
 int main(void){
-    int n;
-    long int r = -1;
+    int n, r = -1, a;
     cin >> n;
-    vector<long int> o, e;
-    long int a;
+    vector<int> o, e;
     for(int i=0; i<n; ++i){
         cin >> a;
         if(a % 2 == 0) e.push_back(a);
@@ -20,4 +18,5 @@ int main(void){
     if(o.size() >= 2) r = max(r, o[0]+o[1]);
     if(e.size() >= 2) r = max(r, e[0]+e[1]);
     cout << r << endl;
+    return 0; 
 }
