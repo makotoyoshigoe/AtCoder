42fed711b38f1f4f6565c3d1c50372039cddf84d
makotoyoshigoe <myoshigo0127@gmail.com>
2022-10-11 00:23:16 +0900

    57c

---
+++ ABC/57/c.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int calc(long int a, long int b){
+    long int la = log10(a), lb = log10(b);
+    if(la >= lb) return la+1;
+    else return lb+1;
+}
+
+int main(void){
+    long int n;
+    cin >> n;
+    int r = 100;
+    int rn = sqrt(n);
+    for(double i=1; i<=rn; ++i){
+        double j = n / i;
+        if(j == (long int)j && r > calc(i, j)) r = calc(i, j);
+    }
+    cout << r << endl;
+    return 0;
+}
