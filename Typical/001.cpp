457153b9b906368328e54c83dc66ebfb080d6095
makotoyoshigoe <myoshigo0127@gmail.com>
2022-11-03 08:10:11 +0900

    01途中

---
+++ Typical/001.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int n, l, k;
+    cin >> n >> l >> k;
+    vector<int> a(n, 0);
+    for(int i=0; i<n; ++i) cin >> a[i];
+}
