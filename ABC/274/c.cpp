6aaf5fec4cd0767766dfd5dd49bf4c34500f66ee
makotoyoshigoe <myoshigo0127@gmail.com>
2022-11-02 10:18:06 +0900

    275

--- ABC/274/c.cpp
+++ ABC/274/c.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(void){
     int n;
     cin >> n;
-    vector<int> a(n), b(2*n+1, 0);
+    vector<int> a(n), b(n, 0);
     for(int i=0; i<n; ++i){
         cin >> a[i];
     }
--- ABC/275/b.cpp
+++ ABC/275/b.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main(void){
-    long long a, b, c, d, e, f;
-    
+    long long a, b, c, d, e, f, x, y;
+    long long mod = 998244353;
     cin >> a >> b >> c >> d >> e >> f;
-    a %= 
-    long long r = (((a * b * c)) - (((d * e * f)))) % 998244353;
+    x = ((a % mod) * (b % mod)) % mod;
+    x = ((x % mod) * (c % mod)) % mod;
+    y = ((d % mod) * (e % mod)) % mod;
+    y = ((y % mod) * (f % mod)) % mod;
+    long long r = (x + mod - y) % mod;
     cout << r << endl;
     return 0;
 }
